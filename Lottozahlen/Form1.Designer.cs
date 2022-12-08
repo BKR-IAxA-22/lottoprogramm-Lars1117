9d44fb6ecaeb1e7d10a7f715659f7f5043346110
Lars Stegemann <1058082@bkrnet.de>
2022-12-08 08:02:07 +0100

    programmiert soweit fertig

--- Lottozahlen/Form1.Designer.cs
+++ Lottozahlen/Form1.Designer.cs
@@ -28,81 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button_zahlen = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.auslosen = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.TextBox();
+            this.number3 = new System.Windows.Forms.TextBox();
+            this.number5 = new System.Windows.Forms.TextBox();
+            this.number4 = new System.Windows.Forms.TextBox();
+            this.number1 = new System.Windows.Forms.TextBox();
+            this.number6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button_zahlen
+            // auslosen
             // 
-            this.button_zahlen.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.button_zahlen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_zahlen.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.button_zahlen.Location = new System.Drawing.Point(201, 96);
-            this.button_zahlen.Name = "button_zahlen";
-            this.button_zahlen.Size = new System.Drawing.Size(327, 80);
-            this.button_zahlen.TabIndex = 1;
-            this.button_zahlen.Text = "Lottozahlen auslosen";
-            this.button_zahlen.UseVisualStyleBackColor = false;
-            this.button_zahlen.Click += new System.EventHandler(this.button_zahlen_Click);
+            this.auslosen.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.auslosen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.auslosen.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.auslosen.Location = new System.Drawing.Point(201, 96);
+            this.auslosen.Name = "auslosen";
+            this.auslosen.Size = new System.Drawing.Size(327, 80);
+            this.auslosen.TabIndex = 1;
+            this.auslosen.Text = "Lottozahlen auslosen";
+            this.auslosen.UseVisualStyleBackColor = false;
+            this.auslosen.Click += new System.EventHandler(this.button_auslosen_Click);
             // 
-            // textBox2
+            // number2
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(233, 211);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(53, 27);
-            this.textBox2.TabIndex = 3;
+            this.number2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.number2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number2.Location = new System.Drawing.Point(234, 335);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(53, 27);
+            this.number2.TabIndex = 3;
             // 
-            // textBox6
+            // number3
             // 
-            this.textBox6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(303, 211);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(53, 27);
-            this.textBox6.TabIndex = 7;
+            this.number3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.number3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number3.Location = new System.Drawing.Point(304, 335);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(53, 27);
+            this.number3.TabIndex = 7;
             // 
-            // textBox1
+            // number5
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(448, 211);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(53, 27);
-            this.textBox1.TabIndex = 8;
+            this.number5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.number5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number5.Location = new System.Drawing.Point(449, 335);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(53, 27);
+            this.number5.TabIndex = 8;
             // 
-            // textBox3
+            // number4
             // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(380, 211);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(53, 27);
-            this.textBox3.TabIndex = 9;
+            this.number4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.number4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number4.Location = new System.Drawing.Point(381, 335);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(53, 27);
+            this.number4.TabIndex = 9;
             // 
-            // textBox4
+            // number1
             // 
-            this.textBox4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(162, 211);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(53, 27);
-            this.textBox4.TabIndex = 10;
+            this.number1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.number1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number1.Location = new System.Drawing.Point(163, 335);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(53, 27);
+            this.number1.TabIndex = 10;
             // 
-            // textBox5
+            // number6
             // 
-            this.textBox5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(517, 211);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(53, 27);
-            this.textBox5.TabIndex = 11;
+            this.number6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.number6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number6.Location = new System.Drawing.Point(518, 335);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(53, 27);
+            this.number6.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(378, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(446, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(515, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 17;
             // 
             // Form1
             // 
@@ -110,13 +164,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(740, 417);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button_zahlen);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.number1);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.auslosen);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -126,13 +186,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button_zahlen;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button auslosen;
+        private System.Windows.Forms.TextBox number2;
+        private System.Windows.Forms.TextBox number3;
+        private System.Windows.Forms.TextBox number5;
+        private System.Windows.Forms.TextBox number4;
+        private System.Windows.Forms.TextBox number1;
+        private System.Windows.Forms.TextBox number6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Lottozahlen/Form1.cs
+++ Lottozahlen/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +17,84 @@ namespace Lottozahlen
         {
             InitializeComponent();
         }
+        int[] numbers = new int[6];
+        Random random = new Random();
 
-        private void button_zahlen_Click(object sender, EventArgs e)
+        private void button_auslosen_Click(object sender, EventArgs e)
         {
-            int[] zahlen = new int[6];
-            Random r = new Random();
-            for(int i = 0; i < zahlen.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                zahlen[i] = r;
+                int check = random.Next(1, 49);
+                if (numbers.Contains(check))
+                {
+                    i--;
+                }
+                else
+                {
+                    numbers[i] = check;
+                }
+            }
+            label1.Text = numbers[0].ToString();
+            label2.Text = numbers[1].ToString();
+            label3.Text = numbers[2].ToString();
+            label4.Text = numbers[3].ToString();
+            label5.Text = numbers[4].ToString();
+            label6.Text = numbers[5].ToString();
+
+            if (numbers.Contains(Convert.ToInt32(number1.Text)))
+            {
+                number1.BackColor = Color.Green;
+            }
+            else
+            {
+                number1.BackColor = Color.White;
             }
 
+            if (numbers.Contains(Convert.ToInt32(number2.Text)))
+            {
+                number2.BackColor = Color.Green;
+            }
+            else
+            {
+                number2.BackColor = Color.White;
+            }
+
+            if (numbers.Contains(Convert.ToInt32(number3.Text)))
+            {
+                number3.BackColor = Color.Green;
+            }
+            else
+            {
+                number3.BackColor = Color.White;
+            }
+
+            if (numbers.Contains(Convert.ToInt32(number4.Text)))
+            {
+                number4.BackColor = Color.Green;
+            }
+            else
+            {
+                number4.BackColor = Color.White;
+            }
+
+            if (numbers.Contains(Convert.ToInt32(number5.Text)))
+            {
+                number5.BackColor = Color.Green;
+            }
+            else
+            {
+                number5.BackColor = Color.White;
+            }
+
+            if (numbers.Contains(Convert.ToInt32(number6.Text)))
+            {
+                number6.BackColor = Color.Green;
+            }
+            else
+            {
+                number6.BackColor = Color.White;
+            }
         }
     }
 }
+
