a60e04c113167f55fd4ff1fb57fbdd456b9402b2
Lars Stegemann <1058082@bkrnet.de>
2022-12-08 08:16:11 +0100

    Design überarbeitet

--- Lottozahlen/Form1.Designer.cs
+++ Lottozahlen/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // auslosen
             // 
-            this.auslosen.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.auslosen.BackColor = System.Drawing.Color.Blue;
             this.auslosen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.auslosen.ForeColor = System.Drawing.SystemColors.Desktop;
             this.auslosen.Location = new System.Drawing.Point(201, 96);
@@ -60,109 +60,121 @@
             // 
             this.number2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.number2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.number2.Location = new System.Drawing.Point(234, 335);
+            this.number2.Location = new System.Drawing.Point(229, 287);
             this.number2.Name = "number2";
             this.number2.Size = new System.Drawing.Size(53, 27);
             this.number2.TabIndex = 3;
+            this.number2.Text = "0";
             // 
             // number3
             // 
             this.number3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.number3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.number3.Location = new System.Drawing.Point(304, 335);
+            this.number3.Location = new System.Drawing.Point(299, 287);
             this.number3.Name = "number3";
             this.number3.Size = new System.Drawing.Size(53, 27);
             this.number3.TabIndex = 7;
+            this.number3.Text = "0";
             // 
             // number5
             // 
             this.number5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.number5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.number5.Location = new System.Drawing.Point(449, 335);
+            this.number5.Location = new System.Drawing.Point(444, 287);
             this.number5.Name = "number5";
             this.number5.Size = new System.Drawing.Size(53, 27);
             this.number5.TabIndex = 8;
+            this.number5.Text = "0";
             // 
             // number4
             // 
             this.number4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.number4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.number4.Location = new System.Drawing.Point(381, 335);
+            this.number4.Location = new System.Drawing.Point(376, 287);
             this.number4.Name = "number4";
             this.number4.Size = new System.Drawing.Size(53, 27);
             this.number4.TabIndex = 9;
+            this.number4.Text = "0";
             // 
             // number1
             // 
             this.number1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.number1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.number1.Location = new System.Drawing.Point(163, 335);
+            this.number1.Location = new System.Drawing.Point(158, 287);
             this.number1.Name = "number1";
             this.number1.Size = new System.Drawing.Size(53, 27);
             this.number1.TabIndex = 10;
+            this.number1.Text = "0";
             // 
             // number6
             // 
             this.number6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.number6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.number6.Location = new System.Drawing.Point(518, 335);
+            this.number6.Location = new System.Drawing.Point(513, 287);
             this.number6.Name = "number6";
             this.number6.Size = new System.Drawing.Size(53, 27);
             this.number6.TabIndex = 11;
+            this.number6.Text = "0";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(163, 206);
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(174, 208);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.Size = new System.Drawing.Size(26, 20);
             this.label1.TabIndex = 12;
+            this.label1.Text = " ";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(231, 206);
+            this.label2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(242, 208);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.Size = new System.Drawing.Size(26, 20);
             this.label2.TabIndex = 13;
+            this.label2.Text = " ";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(301, 206);
+            this.label3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(312, 208);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.Size = new System.Drawing.Size(26, 20);
             this.label3.TabIndex = 14;
+            this.label3.Text = " ";
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(378, 206);
+            this.label4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(389, 208);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.Size = new System.Drawing.Size(26, 20);
             this.label4.TabIndex = 15;
+            this.label4.Text = " ";
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(446, 206);
+            this.label5.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(457, 208);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.Size = new System.Drawing.Size(26, 20);
             this.label5.TabIndex = 16;
+            this.label5.Text = " ";
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(515, 206);
+            this.label6.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(526, 208);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.Size = new System.Drawing.Size(26, 20);
             this.label6.TabIndex = 17;
+            this.label6.Text = " ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(740, 417);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
